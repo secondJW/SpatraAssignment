c3c025c0b2fcbeede172d8dcad649ae09e2787a5
juyeon <sccermoto@gmail.com>
2024-05-01 05:31:14 +0900

    Level1-2

--- src/App.java
+++ src/App.java
@@ -4,7 +4,11 @@ public class App {
         Scanner sc=new Scanner(System.in);
 
         System.out.println("첫 번째 숫자를 입력하세요");
+        int firstNum=sc.nextInt();
         System.out.println("두 번째 숫자를 입력하세요");
+        int secondNum=sc.nextInt();
+        System.out.println("사칙연산 기호를 입력하세요");
+        char sign = sc.nextLine().charAt(0);
 
     }
 }
