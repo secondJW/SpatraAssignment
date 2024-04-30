837725aded1bb2b924d149f616f7a61f3653ecc0
juyeon <sccermoto@gmail.com>
2024-05-01 05:22:25 +0900

    Level1-1

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요");
+        System.out.println("두 번째 숫자를 입력하세요");
+
+    }
+}
