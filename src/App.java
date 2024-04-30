2e17c752f220fbef9ea21eb382100944e59c5107
juyeon <sccermoto@gmail.com>
2024-05-01 06:08:06 +0900

    Level1-4

--- src/App.java
+++ src/App.java
@@ -2,30 +2,41 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String input="";
 
-        System.out.println("첫 번째 숫자를 입력하세요");
-        int firstNum=sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요");
-        int secondNum=sc.nextInt();
-        System.out.println("사칙연산 기호를 입력하세요");
-        char sign = sc.nextLine().charAt(0);
+        while(!input.equals("exit")) {
+            System.out.println("첫 번째 숫자를 입력하세요");
+            int firstNum = sc.nextInt();
+            System.out.println("두 번째 숫자를 입력하세요");
+            int secondNum = sc.nextInt();
+            sc.nextLine(); //nextInt()가 남겨뒀던 개행(\n)을 가져옴
+            System.out.println("사칙연산 기호를 입력하세요");
+            char sign = sc.nextLine().charAt(0);
 
-        int result=0;
+            int result = 0;
 
-        switch (sign){
-            case '+': result=firstNum+secondNum;
-            break;
-            case '-': result=firstNum-secondNum;
-            break;
-            case '*': result=firstNum*secondNum;
-            break;
-            case '/':result=firstNum/secondNum;
-            break;
-            case '%':result=firstNum%secondNum;
-            break;
-            default:
-                System.out.println("다시 입력");
+            switch (sign) {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+                case '/':
+                    result = firstNum / secondNum;
+                    break;
+                case '%':
+                    result = firstNum % secondNum;
+                    break;
+                default:
+                    System.out.println("다시 입력");
+            }
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            input=sc.nextLine();
         }
-        System.out.println("결과: " + result);
     }
 }
