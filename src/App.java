d812a105e2c54bd0ed802c413f7bcaaa055ce13e
juyeon <sccermoto@gmail.com>
2024-05-01 11:02:32 +0900

    Level2-3

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -8,7 +8,6 @@ public class App {
 
         Calculator cal=new Calculator();
 
-        Stack<Integer> stackInt=new Stack<>();
 
         while(!input2.equals("exit")) {
             System.out.println("첫 번째 숫자를 입력하세요");
@@ -20,18 +19,16 @@ public class App {
             char sign = sc.nextLine().charAt(0);
             cal.calculate(firstNum,secondNum,sign);
 
-            stackInt.push(cal.result);
-            // index++;
-            System.out.println("결과: " + cal.result);
+            System.out.println("결과: " + cal.getResult());
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (reomve 입력 시 삭제)");
             input1=sc.nextLine();
             if(input1.equals("remove")){
-                stackInt.pop();
+                cal.stackInt.pop();
             }
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             input3=sc.nextLine();
             if(input3.equals("inquiry")){
-                System.out.println(stackInt);
+                System.out.println(cal.stackInt);
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             input2=sc.nextLine();
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,35 +1,36 @@
+import java.util.Stack;
+
 public class Calculator {
-    int result;
+    private int result;
+    Stack<Integer> stackInt = new Stack<>();
+    public Stack<Integer> getResult(){
+        return stackInt;
+    }
+
+    public void setResult(Stack<Integer> stackInt){
+        this.stackInt=stackInt;
+    }
+
     public double calculate(int firstNum, int secondNum, char sign){
         //level2-1
         result=0;
         try {
 
             switch (sign) {
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-                case '*':
-                    result = firstNum * secondNum;
-                    break;
-                case '/':
-                    result = firstNum / secondNum;  //throw ArtithmeticException
-                    break;
-                case '%':
-                    result = firstNum % secondNum;
-                    break;
-                default:
-                    throw new CalException();
+                case '+'-> result = firstNum + secondNum;
+                case '-'-> result = firstNum - secondNum;
+                case '*'-> result = firstNum * secondNum;
+                case '/'-> result = firstNum / secondNum;  //throw ArtithmeticException
+                case '%'-> result = firstNum % secondNum;
+                default-> throw new CalException();
             }
         }catch (ArithmeticException e){
             System.out.println(e.getMessage());
         }catch (CalException e){
             System.out.println(e.getMessage());
         }
-        //
+        stackInt.push(result);
+
         return result;
     }
 }
